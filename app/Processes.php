33147296518c4b6bf373813db0b8c36673df5903
TranVietHuy <huytran161297@gmail.com>
2020-06-20 17:50:19 +0700

    handle assign for one person and more departments and roles

--- app/Processes.php
+++ app/Processes.php
@@ -8,7 +8,7 @@ use phpDocumentor\Reflection\Element;
 class Processes extends Model
 {
     protected $fillable = [
-        'code' ,'name', 'description', 'xml', 'type', 'deadline', 'update_at','admin_id', 'document'
+        'code' ,'name', 'description', 'xml', 'type', 'collabration', 'deadline', 'update_at','admin_id', 'document'
     ];
 
     public $table = "processes";
